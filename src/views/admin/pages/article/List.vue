fc0cb6a1e073b620fd5c815490131c9984e4fbd9
dmf-code <1015814408@qq.com>
2020-03-16 13:51:10 +0800

    feat: 添加文章编辑功能

--- src/views/admin/pages/article/List.vue
+++ src/views/admin/pages/article/List.vue
@@ -1,41 +1,78 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="日期" width="180"></el-table-column>
-    <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-    <el-table-column prop="address" label="地址"></el-table-column>
-  </el-table>
+  <div>
+    <el-button @click="handleAdd()">添加</el-button>
+    <Add ref="add"></Add>
+    <el-divider></el-divider>
+    <el-table :data="this.tableData" style="width: 100%">
+      <el-table-column label="ID" width="180">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row.id }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="名称" width="180">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row.title }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+      <Update ref="update"></Update>
+    </el-table>
+  </div>
 </template>
 
 <script>
+import Update from "@/views/admin/pages/article/Update";
+import Add from "@/views/admin/pages/article/Add";
 export default {
+  mounted() {
+    this.getTbaleData();
+  },
+  computed: {},
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      tableData: null
     };
+  },
+  methods: {
+    getTbaleData() {
+      this.axios.get("/api/backend/article").then(response => {
+        if (response.data.status == true) {
+          this.tableData = response.data.data.list;
+        }
+      });
+    },
+    handleAdd() {
+      this.$refs.add.dialogFormVisible = true;
+    },
+    handleEdit(index, row) {
+      this.$refs.update.dialogFormVisible = true;
+      row.checkedCategorys = row.checked_categorys;
+      row.checkedTags = row.checked_tags.split(",");
+      console.log("row: ", row);
+      this.$refs.update.form = row;
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      this.axios.delete("/api/backend/article/" + row.id).then(res => {
+        if (res.data.status == true) {
+          this.getTbaleData();
+          this.$message({ message: "删除成功", type: "success" });
+        } else {
+          this.$message.error("删除失败");
+        }
+      });
+      console.log(index, row);
+    }
+  },
+  components: {
+    Update,
+    Add
   }
 };
 </script>
-
-<style>
-</style>
--- src/views/admin/pages/article/Update.vue
+++ src/views/admin/pages/article/Update.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-dialog title="添加文章" :visible.sync="dialogFormVisible" :append-to-body="true">
+  <el-dialog title="编辑文章" :visible.sync="dialogFormVisible" :append-to-body="true">
     <el-form ref="form" :model="form" label-width="80px">
       <el-form-item label="标题">
         <el-input v-model="form.title"></el-input>
@@ -7,7 +7,7 @@
       <el-form-item label="分类">
         <el-select v-model="form.checkedCategorys" placeholder="请选择">
           <el-option
-            v-for="category in form.categorys"
+            v-for="category in categorys"
             :key="category.id"
             :label="category.name"
             :value="category.id"
@@ -17,7 +17,8 @@
 
       <el-form-item label="标签">
         <el-checkbox-group v-model="form.checkedTags">
-          <el-checkbox v-for="tag in form.tags" :label="tag.id" :key="tag.id">{{tag.name}}</el-checkbox>
+          {{ tags }}
+          <el-checkbox v-for="tag in tags" :label="tag.id" :key="tag.id">{{tag.name}}</el-checkbox>
         </el-checkbox-group>
       </el-form-item>
       <el-form-item label="内容">
@@ -33,9 +34,41 @@
 
 <script>
 export default {
+  created() {},
   mounted() {
     this.getCategorys();
     this.getTags();
+    console.log("category: ", this.categorys);
+    console.log("tags: ", this.tags);
+    console.log("created: ", this.form);
+  },
+  watch: {
+    form(val, oldVal) {
+      console.log("form val: ", val);
+      console.log("form oldVal: ", oldVal);
+    },
+    categorys(val, oldVal) {
+      console.log("categorys val: ", val);
+      console.log("categorys oldVal: ", oldVal);
+    },
+    tags(val, oldVal) {
+      console.log("tags val: ", val);
+      console.log("tags oldVal: ", oldVal);
+    }
+  },
+  data() {
+    return {
+      dialogFormVisible: false,
+      categorys: [],
+      tags: [],
+      form: {
+        title: "",
+        checkedCategorys: [],
+        checkedTags: [],
+        mdCode: "",
+        htmlCode: ""
+      }
+    };
   },
   methods: {
     edit(value, render) {
@@ -66,38 +99,21 @@ export default {
     getCategorys() {
       this.axios.get("/api/backend/category").then(res => {
         if (res.data.status == true) {
-          this.form.categorys = res.data.data.list;
+          this.categorys = res.data.data.list;
         } else {
-          this.form.categorys = [];
+          this.categorys = [];
         }
       });
     },
     getTags() {
       this.axios.get("/api/backend/tag").then(res => {
         if (res.data.status == true) {
-          this.form.tags = res.data.data.list;
+          this.tags = res.data.data.list;
         } else {
-          this.form.tags = [];
+          this.tags = [];
         }
       });
     }
-  },
-  data: function() {
-    return {
-      dialogFormVisible: false,
-      form: {
-        title: "",
-        checkedCategorys: [],
-        categorys: "",
-        checkedTags: [],
-        tags: "",
-        mdCode: "",
-        htmlCode: ""
-      }
-    };
   }
 };
 </script>
-
-<style>
-</style>
