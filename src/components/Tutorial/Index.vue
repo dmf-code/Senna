cc8a307673c066e9afb5d2d12c74093b6f42e6a6
dengminfeng <1015814408@qq.com>
2020-08-15 23:04:53 +0800

    feat: 添加教程骨架

---
+++ src/components/Tutorial/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-row>
+    <el-col></el-col>
+    <el-col>
+      <router-view></router-view>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
