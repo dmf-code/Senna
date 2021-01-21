402bc277bd8e11d352e861e857883a90af6d8a5a
deng minfeng <1015814408@qq.com>
2021-01-21 11:10:20 +0800

    fix: 修复未登录无法渲染用户名问题

--- src/views/layouts/Header.vue
+++ src/views/layouts/Header.vue
@@ -3,11 +3,11 @@
     <el-menu router :default-active="$route.path" class="el-menu-demo" mode="horizontal">
       <el-menu-item index="/">梧桐树下</el-menu-item>
       <el-menu-item index="/tutorial">文档中心</el-menu-item>
-      <el-menu-item index="/login" style="float: right;" v-if="this.user_info == null">
+      <el-menu-item index="/login" style="float: right;" v-if="user_info.user == null">
         <span>登录/注册</span>
       </el-menu-item>
-      <el-submenu index style="float:right;" v-else-if="this.user_info != null">
-        <template slot="title" v-if="user_info.user">{{ user_info.user.username }}</template>
+      <el-submenu index style="float:right;" v-else-if="user_info.user != null">
+        <template slot="title">{{ user_info.user.username }}</template>
         <el-menu-item index="/front/user/center">用户中心</el-menu-item>
         <el-menu-item index="/admin/dashboard">后台首页</el-menu-item>
         <el-menu-item @click="handleLogout">退出登录</el-menu-item>
