7880fc52095cd7bba73169811b3a67c845dfbcfc
deng minfeng <1015814408@qq.com>
2021-01-21 11:08:46 +0800

    fix: 修复未登录无法渲染用户名问题

--- src/views/layouts/Header.vue
+++ src/views/layouts/Header.vue
@@ -7,7 +7,7 @@
         <span>登录/注册</span>
       </el-menu-item>
       <el-submenu index style="float:right;" v-else-if="this.user_info != null">
-        <template slot="title">{{ user_info.user.username }}</template>
+        <template slot="title" v-if="user_info.user">{{ user_info.user.username }}</template>
         <el-menu-item index="/front/user/center">用户中心</el-menu-item>
         <el-menu-item index="/admin/dashboard">后台首页</el-menu-item>
         <el-menu-item @click="handleLogout">退出登录</el-menu-item>
